ffeed9b2e76f99fa6404c52980d666e027b8db69
alex nham <alexnham11@gmail.com>
2024-03-25 14:30:27 -0400

    working player

--- Scripts/enemy_melee.gd
+++ Scripts/enemy_melee.gd
@@ -9,20 +9,20 @@ var player_position
 signal die_event
 
 func _ready():
-    main_char = get_node("../Player")
-    connect("die_event",die)
+	main_char = get_node("../Player")
+	connect("die_event",die)
 
 func die() -> void:
-    queue_free()
+	queue_free()
 
 func _physics_process(delta: float) -> void:
-    if(position != main_char.position):
-        position = position.move_toward(main_char.position,SPEED);
+	if(position != main_char.position):
+		position = position.move_toward(main_char.position,SPEED);
 
 
 func _on_body_entered(body: Node2D) -> void:
-    if body.is_in_group("Slash"):
-        die_event.emit()
-    if body.is_in_group("Player"):
-        print("ouch")
-        main_char.emit_signal("enemy_damage", .1)
+	if body.is_in_group("Slash"):
+		die_event.emit()
+	if body.is_in_group("Player"):
+		print("ouch")
+		main_char.emit_signal("enemy_damage", .1)
