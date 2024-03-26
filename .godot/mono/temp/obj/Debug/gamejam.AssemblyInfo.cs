6028284e3423831fb654d7193910fce2eaa5fbfc
Thomas Vu <samduykhanh01@gmail.com>
2024-03-26 15:35:47 -0400

    Put HUD to StartingScene

--- .godot/mono/temp/obj/Debug/gamejam.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/gamejam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce36a61a514fe00cb61a8e63315ff5785ce45e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f155a53dbc8e8e4af5c5c177a8233f40ea6953")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
