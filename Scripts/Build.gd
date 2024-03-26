523159b4b5d24381cf60f2c9348221a73445c236
alex nham <alexnham11@gmail.com>
2024-03-26 19:02:45 -0400

    upgrading background

--- Scripts/Build.gd
+++ Scripts/Build.gd
@@ -7,16 +7,16 @@ var sword: Sprite2D
 var attack: CharacterBody2D
 
 func _ready():
-    slash = preload("res://Scenes/slash.tscn")
-    sword = $Player/Weapon
-    connect("summon_attack", spawn_attack)
+	slash = preload("res://Scenes/slash.tscn")
+	sword = $Player/Weapon
+	connect("summon_attack", spawn_attack)
 
 func spawn_attack() -> void:
-    print("hello")
-    attack = slash.instantiate()
-    attack.global_position = sword.global_position
-    attack.scale = sword.scale
-    add_child(attack)
+	print("hello")
+	attack = slash.instantiate()
+	attack.global_position = sword.global_position
+	attack.scale = sword.scale
+	add_child(attack)
 
 func _process(delta: float) -> void:
-    pass
+	pass
