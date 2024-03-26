ce36a61a514fe00cb61a8e63315ff5785ce45e79
Thomas Vu <samduykhanh01@gmail.com>
2024-03-26 14:56:52 -0400

    ADDED GDSCRIPT FOR HUD

--- .godot/mono/temp/obj/Debug/gamejam.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/gamejam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928206cbbb8112415aec1cd4ef22688adef6a4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eaec2a738ad9000cc2d536b735cb4658679891f")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamejam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
