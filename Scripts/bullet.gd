ca248b409dc983627c5a7230201cfecb140d205b
alex nham <alexnham11@gmail.com>
2024-03-25 21:23:10 -0400

    bullet damage

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -3,42 +3,43 @@ extends CharacterBody2D
 #signal hit
 var bulletVelocity = Vector2(1, 0)
 var speed = 500
+var main_char: CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	main_char = get_node("../../Player")
+	pass # Replace with function body.
 
 func _physics_process(delta):
-    # If node type is changed to area2d, then change move and collide to move towards
-    var collision_info = move_and_collide(bulletVelocity.normalized() * delta * speed)
-    if collision_info:
-       print("Bullet collided with ", collision_info.get_collider().name)
-    #velocity = velocity.slide(bulletVelocity.normalized() * delta * speed)
-    #global_position = global_position.move_toward(bulletVelocity, delta)
-    #pass
-    
+	# If node type is changed to area2d, then change move and collide to move towards
+	var collision_info = move_and_collide(bulletVelocity.normalized() * delta * speed)
+	if collision_info:
+		print("Bullet collided with ", collision_info.get_collider().name)
+	#velocity = velocity.slide(bulletVelocity.normalized() * delta * speed)
+	#global_position = global_position.move_toward(bulletVelocity, delta)
+	#pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 #func _on_hit():
-    ##pass # Replace with function body.
-    #visible = false
-    #queue_free()
+	##pass # Replace with function body.
+	#visible = false
+	#queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_entered():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 
 
 func _on_area_2d_body_entered(body):
-    queue_free()
-    #pass # Replace with function body.
+	queue_free()
+	if body.is_in_group("Player"):
+		print("ouch")
+		main_char.emit_signal("enemy_damage", 1)
+	#pass # Replace with function body.
 
-
-func _on_area_2d_area_entered(area):
-    queue_free()
-    #pass # Replace with function body.
--- Scripts/emeny_shooting.gd
+++ Scripts/emeny_shooting.gd
@@ -9,73 +9,73 @@ var reloadTime
 signal shot
 
 func _ready():
-    _mainChar = get_node("../Player")
-    #_bullet = get_node("Bullet")
-    _bulletScene = preload("res://Scenes/bullet.tscn")
-    reloadTime = get_node("ReloadTime")
-    
-    reloadTime.wait_time = 1
-    reloadTime.start()
-    #shot = false
-    #reloadTime.stop()
-    #reloadTime.start()
+	_mainChar = get_node("../Player")
+	#_bullet = get_node("Bullet")
+	_bulletScene = preload("res://Scenes/bullet.tscn")
+	reloadTime = get_node("ReloadTime")
+	
+	reloadTime.wait_time = 1
+	reloadTime.start()
+	#shot = false
+	#reloadTime.stop()
+	#reloadTime.start()
 
 
 var speed = 1.0
 func _physics_process(delta):
-    print(_mainChar.global_position)
-    
-    # Shooting enemy move towards current position of main character
-    global_position = global_position.move_toward(_mainChar.global_position, speed)
-    
-    
-    # Bullet move towards where main character was
-    #if shot == true:
-        #_bullet.global_position = global_position.move_toward(_mainChar.global_position, speed * 5)
-        
-    
-    #pass
-    
+
+	
+	# Shooting enemy move towards current position of main character
+	global_position = global_position.move_toward(_mainChar.global_position, speed)
+	
+	
+	# Bullet move towards where main character was
+	#if shot == true:
+		#_bullet.global_position = global_position.move_toward(_mainChar.global_position, speed * 5)
+		
+	
+	#pass
+	
 func _process(delta):
-    get_node("Node2D").look_at(_mainChar.global_position)
-    #pass
-    
+	get_node("Node2D").look_at(_mainChar.global_position)
+	#pass
+	
 #func shoot():
-    #var bullet = _bulletScene.instantiate()
-    #add_child(bullet)
-    #bullet.global_position = $Node2D/BulletSpawn.global_position
-    #bullet.bulletVelocity = $Node2D/BulletSpawn.global_position - bullet.position
+	#var bullet = _bulletScene.instantiate()
+	#add_child(bullet)
+	#bullet.global_position = $Node2D/BulletSpawn.global_position
+	#bullet.bulletVelocity = $Node2D/BulletSpawn.global_position - bullet.position
 
 func _on_visible_on_screen_notifier_2d_screen_entered():
-    #reloadTime.start()
-    pass
-    #pass # Replace with function body.
+	#reloadTime.start()
+	pass
+	#pass # Replace with function body.
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-    #reloadTime.stop()
-    pass # Replace with function body.
+	#reloadTime.stop()
+	pass # Replace with function body.
 
 
 func _on_reload_time_timeout():
-    #if _bullet.global_position != global_position:
-    emit_signal("shot")
-    #print("Bullet global position = ", _bullet.global_position)
-    print("enemy_shooting global position = ", global_position)
-    #pass # Replace with function body.
+	#if _bullet.global_position != global_position:
+	emit_signal("shot")
+	#print("Bullet global position = ", _bullet.global_position)
+	print("enemy_shooting global position = ", global_position)
+	#pass # Replace with function body.
 
 
 func _on_shot():
-    #if _bullet.global_position != global_position:
-    #_bullet.visible = true
-    #shot = true
-    #add_child(_bullet)
-    #print("Added bullet to enemy shooting")
-    #pass # Replace with function body.
-    var bullet = _bulletScene.instantiate()
-    add_child(bullet)
-    bullet.global_position = $Node2D/BulletSpawn.global_position
-    bullet.bulletVelocity = _mainChar.global_position - bullet.global_position
-    #pass
+	#if _bullet.global_position != global_position:
+	#_bullet.visible = true
+	#shot = true
+	#add_child(_bullet)
+	#print("Added bullet to enemy shooting")
+	#pass # Replace with function body.
+	var bullet = _bulletScene.instantiate()
+	add_child(bullet)
+	bullet.global_position = $Node2D/BulletSpawn.global_position
+	bullet.bulletVelocity = _mainChar.global_position - bullet.global_position
+	#pass
 
 
